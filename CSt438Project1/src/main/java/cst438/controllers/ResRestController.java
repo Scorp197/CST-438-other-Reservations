d6fe4c37bfcb32def05fe4d458a78694a2769b2c
Brian James <54527474+brian-lo0-127001@users.noreply.github.com>
2021-06-08 02:11:44 -0700

    Update ResRestController.java

--- CSt438Project1/src/main/java/cst438/controllers/ResRestController.java
+++ CSt438Project1/src/main/java/cst438/controllers/ResRestController.java
@@ -1,6 +1,18 @@
 package cst438.controllers;
 
-public class ResRestController
-{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import cst438.services.PackageService;
 
+@RestController
+@RequestMapping("/api")
+public class ResRestController 
+{
+  @Autowired
+  private PackageService packageService;
 }
