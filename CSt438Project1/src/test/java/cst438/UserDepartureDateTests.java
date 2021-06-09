fbe4c9f13ece031beb6c2953a488a770094a365f
Christal O'Connell <32943793+Scorp197@users.noreply.github.com>
2021-06-08 21:34:43 -0700

    Add files via upload

---
+++ CSt438Project1/src/test/java/cst438/UserDepartureDateTests.java
@@ -0,0 +1,18 @@
+package cst438;
+
+
+
+
+import org.junit.jupiter.api.Test;
+
+import cst438.domain.UserDepartureDate;
+
+public class UserDepartureDateTests {
+	
+	@Test
+	void Test2() {
+		
+	new UserDepartureDate("2021-May-22");
+
+	}
+}
