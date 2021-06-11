e475a91519c818a4fb25fc672f8e208d7e7da681
Brian James <54527474+brian-lo0-127001@users.noreply.github.com>
2021-06-10 18:16:20 -0700

    Update User.java
    Non nulls were removed in some cases

--- CSt438Project1/src/main/java/cst438/domain/User.java
+++ CSt438Project1/src/main/java/cst438/domain/User.java
@@ -1,7 +1,6 @@
 package cst438.domain;
 
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
@@ -9,104 +8,97 @@ import javax.validation.constraints.Size;
 
 @Entity
 public class User {
-	
-	@Id
-	@GeneratedValue
-	private long id;
-	
-	@NotNull
-	@Size(min=3, max=25)
-	private String Username;
-	
-	@NotNull
-	@Size(min=3, max=25)
-	private String Password;
-	
-	
-	@NotNull
-	@Size(min=3, max=25)
-	private String firstName;
-	
-	@NotNull
-	@Size(min=3, max=25)
-	private String lastName;
-	
 
-//	@NotNull
-//	@Size(min=3, max=25)
-//	private String originCity;
-//	
-	
-	private String date;
-	
-	
-	public User() {
-		
-	}
-	
-	public User(@NotNull int id, String Username, String Password, String firstName, String lastName) {
-		super();
-		this.id = id;
-		this.Username = Username;
-		this.Password = Password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-//		this.originCity = originCity;
-		
-	}
-	
+   @Id
+   @GeneratedValue
+   private long id;
 
-//	public String getOriginCity() {
-//		return originCity;
-//	}
-//
-//	public void setOriginCity(String originCity) {
-//		this.originCity = originCity;
-//	}
+   //@NotNull
+   @Size(min=3, max=25)
+   private String Username;
 
-	public String getFirstName() {
-		return firstName;
-	}
+   //@NotNull
+   @Size(min=3, max=25)
+   private String Password;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+   //@NotNull
+   @Size(min=3, max=25)
+   private String firstName;
 
-	public String getLastName() {
-		return lastName;
-	}
+   //@NotNull
+   @Size(min=3, max=25)
+   private String lastName;
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+   //  @NotNull
+   //  @Size(min=3, max=25)
+   //  private String originCity;
+   //
 
-	public long getId() { return id; }
-	public void setId(long id) { this.id = id;}
+   private String date;
 
-	public String getUsername() {
-		return Username;
-	}
+   public User() {
 
-	public void setUsername(String Username) {
-		this.Username = Username;
-	}
+   }
 
-	public String getPassword() {
-		return Password;
-	}
+   public User(@NotNull int id, String Username, String Password, String originCity, String destination, String departureDate, String returnDate) {
+      super();
+      this.id = id;
+      this.Username = Username;
+      this.Password = Password;
+      this.firstName = firstName;
+      this.lastName = lastName;
+      //      this.originCity = originCity;
 
-	public void setPassword(String Password) {
-		this.Password = Password;
-	}
+   }
 
-	public String getDate() {
-		return date;
-	}
+   //  public String getOriginCity() {
+   //      return originCity;
+   //  }
+   //
+   //  public void setOriginCity(String originCity) {
+   //      this.originCity = originCity;
+   //  }
 
-	public void setDate(String date) {
-		this.date = date;
-	} 
-	
-	
-	
+   public String getDate() {
+      return date;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public long getId() { return id; }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public String getPassword() {
+      return Password;
+   }
+   public String getUsername() {
+      return Username;
+   }
+
+   public void setDate(String date) {
+      this.date = date;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public void setId(long id) { this.id = id;}
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public void setPassword(String Password) {
+      this.Password = Password;
+   }
+
+   public void setUsername(String Username) {
+      this.Username = Username;
+   }
 }
