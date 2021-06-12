cf6b181963c70eae272e2ea4fdfdd3f5eacb85da
Brian James <54527474+brian-lo0-127001@users.noreply.github.com>
2021-06-12 12:51:38 -0700

    Update ResRestController.java
    Added GetMapping for Rest Controller

--- CSt438Project1/src/main/java/cst438/controllers/ResRestController.java
+++ CSt438Project1/src/main/java/cst438/controllers/ResRestController.java
@@ -1,18 +1,53 @@
 package cst438.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
+import cst438.domain.PackageRepository;
 import cst438.services.PackageService;
 
 @RestController
 @RequestMapping("/api")
-public class ResRestController 
+public class ResRestController
 {
-  @Autowired
-  private PackageService packageService;
+   private static final Logger logger = LoggerFactory.getLogger(ResRestController.class);
+
+   @Autowired
+   PackageRepository packageRepository;
+
+   @Autowired
+   private PackageService packageService;
+
+   // http://localhost:8080/api/getCarByCity/seattle
+   @GetMapping(value = "/getCarByCity/{city}")
+   public void getCarByCity(@PathVariable String city)
+   {
+
+      logger.info("Car Service Get Mapping city: {}", city);
+
+   }
+
+   // http://localhost:8080/api/getFlightByCity/seattle/boston
+   @GetMapping(value = "/getFlightByCity/{originCity}/{destination}")
+   public void getFlightByCity(@PathVariable String originCity, @PathVariable String destination)
+   {
+
+      logger.info("Airline Service Get Mapping originCity: {} and destination: {}", originCity, destination);
+
+   }
+   // http://localhost:8080/api/getHotelByCity/seattle
+   @GetMapping(value = "/getHotelByCity/{city}")
+   public void getHotelByCity(@PathVariable String city)
+   {
+
+      logger.info("Hotel Service Get Mapping city: {}", city);
+
+
+   }
+
 }
