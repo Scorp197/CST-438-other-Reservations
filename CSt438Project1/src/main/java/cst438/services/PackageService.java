7786c88b74a7d83118d594cb077e468fc8004d1d
Brian James <54527474+brian-lo0-127001@users.noreply.github.com>
2021-06-16 16:49:09 -0700

    Update PackageService.java
    added new methods for searching

--- CSt438Project1/src/main/java/cst438/services/PackageService.java
+++ CSt438Project1/src/main/java/cst438/services/PackageService.java
@@ -1,5 +1,9 @@
 package cst438.services;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,7 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cst438.domain.CarFind;
 import cst438.domain.FlightFind;
@@ -15,14 +21,12 @@ import cst438.domain.HotelFind;
 
 @Service
 public class PackageService {
-
+   
    private static final Logger log = LoggerFactory.getLogger(PackageService.class);
-
-   private RestTemplate restTemplate;
-
-   private String airlineUrl;
-   private String carUrl;
-   private String hotelUrl;
+   private static RestTemplate restTemplate;
+   private static String carUrl;
+   private static String airlineUrl;
+   private static String hotelUrl;
 
    // retrieve url for PackageService from application.properties file
    public PackageService(
@@ -104,5 +108,83 @@ public class PackageService {
       return new HotelFind(hotelId, hotelName, arrivalDate,
          departureDate, hotelCity, price);
    }
+   
+   public static CarFind searchCar(String carCity)
+   {
 
+      ResponseEntity<JsonNode> response = restTemplate.getForEntity(
+         carUrl + "/api/getCarByCity?city=" + carCity, JsonNode.class);
+
+      JsonNode json = response.getBody();
+      log.info("Status code from car server:" + response.getStatusCodeValue());
+      log.info("GETBODY" + response.getBody());
+
+      final ObjectMapper objectMapper = new ObjectMapper();
+      CarFind[] cars = null;
+      try
+      {
+         cars = objectMapper.readValue(json.toPrettyString(), CarFind[].class);
+      } catch (JsonProcessingException e)
+      {
+         e.printStackTrace();
+      }
+
+      List<CarFind> carList = new ArrayList<CarFind>(Arrays.asList(cars));
+
+      carList.forEach(x -> System.out.println(x.toString()));
+      return carList.get(0);
+   }
+   
+      public static HotelFind searchHotel()
+   {
+
+      ResponseEntity<JsonNode> response = restTemplate.getForEntity(
+         hotelUrl + "/api/hotels",
+         JsonNode.class);
+
+      JsonNode json = response.getBody();
+      log.info("Status code from hotel server:" + response.getStatusCodeValue());
+      log.info("GETBODY" + response.getBody());
+
+      final ObjectMapper objectMapper = new ObjectMapper();
+      HotelFind[] hotels = null;
+      try
+      {
+         hotels = objectMapper.readValue(json.toPrettyString(), HotelFind[].class);
+      }  catch (JsonProcessingException e)
+      {
+         e.printStackTrace();
+      }
+
+      List<HotelFind> hotelList = new ArrayList<HotelFind>(Arrays.asList(hotels));
+
+      hotelList.forEach(x -> System.out.println(x.toString()));
+      return hotelList.get(0);
+   }
+   
+      public static FlightFind searchFlight(String originCity)
+   {
+
+      ResponseEntity<JsonNode> response = restTemplate.getForEntity(
+         "https://cst438w3a-airline-service.herokuapp.com/api/getFlights?originCity=seattle&destinationCity=san%20diego", JsonNode.class);
+
+      JsonNode json = response.getBody();
+      log.info("Status code from airline server:" + response.getStatusCodeValue());
+      log.info("GETBODY" + response.getBody());
+
+      final ObjectMapper objectMapper = new ObjectMapper();
+      FlightFind[] flights = null;
+      try
+      {
+         flights = objectMapper.readValue(json.toPrettyString(), FlightFind[].class);
+      } catch (JsonProcessingException e)
+      {
+         e.printStackTrace();
+      }
+
+      List<FlightFind> flightList = new ArrayList<FlightFind>(Arrays.asList(flights));
+
+      flightList.forEach(x -> System.out.println(x.toString()));
+      return flightList.get(0);
+   }
 }
