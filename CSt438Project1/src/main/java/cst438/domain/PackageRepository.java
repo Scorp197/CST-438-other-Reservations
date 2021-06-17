44c9392f3a850d74bce2074f1bdc28538c842f3e
Christal O'Connell <32943793+Scorp197@users.noreply.github.com>
2021-06-16 17:03:19 -0700

    Add files via upload

--- CSt438Project1/src/main/java/cst438/domain/PackageRepository.java
+++ CSt438Project1/src/main/java/cst438/domain/PackageRepository.java
@@ -1,13 +1,22 @@
 package cst438.domain;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PackageRepository extends CrudRepository<Package, Long> {
+	
 
-   @Query("select p from Package p order by packageId desc")
+   @Query("SELECT p from Package p order by packageId desc")
    List<Package> findAllOrderByPackageDesc();
+   
+   @Query(value = "SELECT * from Package p WHERE p.origin_city = ?1", nativeQuery = true)
+   List<Package> findByoriginCity(String originCity, String destinationCity);
+   
+	 List<Package>findAll();
 }
+//	List<Package> find(long packageId);
+
+//}
